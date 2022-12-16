bf63f8056dbccd150cf642ad0922cf2fa4846cdf
wortzzzzz <lethanhdat762003@gmail.com>
2022-12-16 19:37:23 +0700

    Fix header hide when users logged.

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -1,39 +1,37 @@
 <section class="slider_section ">
-    <div class="slider_bg_box">
-        <img src="home/images/soap-hd-crop.jpg" alt="">
-    </div>
-    <div id="customCarousel1" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6 ">
-                            <div class="detail-box">
-                                <h1 style="color: white;">
-                                    <span>
-<<<<<<< HEAD
-                                        Merry Christmas
-=======
-                                        Sale 20% Off
->>>>>>> 8371016 (Fix product details page.)
-                                    </span>
-                                    <br>
-                                    On Everything
-                                </h1>
-                                <p style="color: white;">
-                                    Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
-                                </p>
-                                <div class="btn-box">
-                                    <a href="" class="btn1">
-                                        Shop Now
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <div class="slider_bg_box">
+    <img src="home/images/soap-hd-crop.jpg" alt="">
+  </div>
+  <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 col-lg-6 ">
+              <div class="detail-box">
+                <h1 style="color: white;">
+                  <span>
+                    Sale 20% Off
+                  </span>
+                  <br>
+                  On Everything
+                </h1>
+                <p style="color: white;">
+                  Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid
+                  error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat
+                  modi impedit sequi.
+                </p>
+                <div class="btn-box">
+                  <a href="" class="btn1">
+                    Shop Now
+                  </a>
                 </div>
+              </div>
             </div>
-            {{-- <div class="carousel-item ">
+          </div>
+        </div>
+      </div>
+      {{-- <div class="carousel-item ">
                 <img src="home/images/bckg-2.jpg" alt="" class="d-block w-100">
                 <div class="carousel-caption container">
                     <div class="row">
@@ -85,13 +83,13 @@
                     </div>
                 </div>
             </div> --}}
-        </div>
-        {{-- <div class="container">
+    </div>
+    {{-- <div class="container">
             <ol class="carousel-indicators">
                 <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
                 <li data-target="#customCarousel1" data-slide-to="1"></li>
                 <li data-target="#customCarousel1" data-slide-to="2"></li>
             </ol>
         </div> --}}
-    </div>
+  </div>
 </section>
